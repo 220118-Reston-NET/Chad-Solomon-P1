3dab94de7be4c014a66e04c7a5be2d0a1945c113
Chad Solomon <chadcsoloman@gmail.com>
2022-02-27 18:49:55 -0500

    Async

--- PokeBL/IOrderBL.cs
+++ PokeBL/IOrderBL.cs
@@ -4,13 +4,15 @@ namespace PokeBL
     public interface IOrderBL
     {
 
-        void AddOrder(int _orderLocation, int _price, int _custID, List<LineItems> _cart);
+        void AddOrder(int _orderLocation, int _custID, List<LineItems> _cart);
 
-        //List<Order> GetAllOrder(int custID);
+        Task<List<Order>> GetAllOrder();
 
         Task<List<Order>> SearchOrder(int custID);
 
         Task<List<Order>> SearchOrderFilter(int p_custID, string p_filter);
+
+
     }
 
 
