93f75bf9f6fd3c22cabf929db58a477144ef372f
Chad Solomon <chadcsoloman@gmail.com>
2022-02-27 18:52:39 -0500

    Added model for order details

---
+++ PokeModel/ProductDetails.cs
@@ -0,0 +1,10 @@
+namespace PokeModel
+{
+    public class ProductDetails
+    {
+
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
