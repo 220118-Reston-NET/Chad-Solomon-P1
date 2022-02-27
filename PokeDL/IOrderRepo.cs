6854d19e6167418327494d6c41ff8d6555a9d945
Chad Solomon <chadcsoloman@gmail.com>
2022-02-27 18:51:09 -0500

    Async

--- PokeDL/IOrderRepo.cs
+++ PokeDL/IOrderRepo.cs
@@ -5,12 +5,13 @@ namespace PokeDL
     public interface IOrderRepo
     {
 
-        public void AddOrder(int _orderLocation, int _price, int _custID, List<LineItems> _cart);
+        public void AddOrder(int _orderLocation, int _custID, List<LineItems> _cart);
 
         public Task<List<Order>> GetAllOrder();
 
         // List<Order> SearchOrder(int custID);
 
         // public List<Order> GetAllOrder();
+        List<LineItems> GetShoppingCartByOrderID(int p_orderID);
     }
 }
