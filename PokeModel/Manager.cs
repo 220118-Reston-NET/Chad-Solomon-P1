d63efc36ddd54f725ee0b1c47b3c9a8b2731ad83
Chad Solomon <chadcsoloman@gmail.com>
2022-03-01 14:32:27 -0500

    first Commit

---
+++ PokeModel/Manager.cs
@@ -0,0 +1,143 @@
+namespace PokeModel
+{
+    public class Manager
+    {
+
+
+        public int _managerID { get; set; }
+        public int ManagerID
+        {
+            get { return _managerID; }
+
+            set
+            {
+                if (value > 0)
+                {
+
+                    _managerID = value;
+                }
+                else
+                {
+
+                    Console.WriteLine("ID must not be empty");
+                    Console.WriteLine("Please press enter!");
+                    Console.ReadLine();
+
+                }
+
+            }
+        }
+
+        private int _storeID;
+        public int StoreFrontID
+
+        {
+            get { return _storeID; }
+            set { _storeID = value; }
+        }
+
+        private string? _managerName; //{ get; set; }
+        public string? ManagerName
+        {
+            get { return _managerName; }
+
+            set
+            {
+                if (value != "")
+                {
+
+                    _managerName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Name cannot be empty");
+                }
+
+            }
+        }
+
+
+
+        private string? _managerAddress; //{ get; set; }
+        public string? ManagerAddress
+        {
+            get { return _managerAddress; }
+
+            set
+            {
+
+                if (value != "")
+                {
+
+                    _managerAddress = value;
+                }
+                else
+                {
+                    Console.WriteLine("Address must not be empty");
+                    Console.WriteLine("Please press enter!");
+                    Console.ReadLine();
+
+                }
+            }
+
+        }
+        private string? _managerEmail;
+        public string? ManagerEmail
+        {
+            get { return _managerEmail; }
+
+            set
+            {
+
+                if (value != "")
+                {
+
+                    _managerEmail = value;
+                }
+                else
+                {
+                    Console.WriteLine("Email must not be empty!");
+                    Console.WriteLine("Please press enter to continue");
+                    Console.ReadLine();
+
+                }
+            }
+
+        }
+
+        private string? _managerPassword;
+        public string? ManagerPassword
+        {
+            get { return _managerPassword; }
+
+            set
+            {
+
+                if (value != "")
+                {
+
+                    _managerPassword = value;
+                }
+                else
+                {
+                    Console.WriteLine("Password must not be empty!");
+                    Console.WriteLine("Please press enter to continue");
+                    Console.ReadLine();
+
+                }
+            }
+
+        }
+
+
+
+
+
+        // public override string ToString()
+        // {
+        //     return $"Name: {Name}\nAddress: {Address}\nEmail: {Email}\nPassword {Password}\n";
+        // }
+
+
+    }
+}
