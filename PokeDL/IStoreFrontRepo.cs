26e44921a58b20cc784d9e0c48e838d360d0162d
Chad Solomon <chadcsoloman@gmail.com>
2022-03-01 14:34:20 -0500

    added updateinventory

--- PokeDL/IStoreFrontRepo.cs
+++ PokeDL/IStoreFrontRepo.cs
@@ -9,9 +9,11 @@ namespace PokeDL
     public interface IStoreFrontRepo
     {
         public StoreFront AddStoreFront(StoreFront s_name);
+        public Inventory AddInventory(Inventory _productID);
 
 
         public List<StoreFront> GetAllStoreFronts();
+        public List<Manager> GetAllManagers();
 
 
 
