7f4c6099c562df9136f97549d0909658f5e7047c
Chad Solomon <chadcsoloman@gmail.com>
2022-03-01 14:35:34 -0500

    made changes but switched everything back

--- PokeBL/IProductsBL.cs
+++ PokeBL/IProductsBL.cs
@@ -8,6 +8,7 @@ namespace PokeBL
         List<Product> SearchProduct(string c_product);
 
         List<Product> GetAllProductByStoreID(int storeID);
+        // List<Product> GetProductByID(int prodID);
 
         Product GetProductByID(int p_prodID);
         List<Product> GetAllProducts();
