405565a16a1249fdfbba1e0938ce33f64fe974a5
Chad Solomon <chadcsoloman@gmail.com>
2022-03-01 14:34:50 -0500

    added update inventory

--- PokeDL/SQLStoreFrontRepo.cs
+++ PokeDL/SQLStoreFrontRepo.cs
@@ -33,40 +33,7 @@ namespace PokeDL
 
         }
 
-        // public List<StoreFront> SearchStoreFrontById(StoreFront storeID)
-        // {
 
-        //     List<StoreFront> listOfStoreFront = new List<StoreFront>();
-
-        //     string sqlQuery = @"select * from StoreFront where storeID = @storeID";
-
-        //     using (SqlConnection con = new SqlConnection("Server=tcp:furrbabies.database.windows.net,1433;Initial Catalog=Furr-Babbies-Pet-Supply;Persist Security Info=False;User ID=FurrBabies;Password=RheaandLdog1$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
-        //     {
-        //         con.Open();
-
-        //         SqlCommand command = new SqlCommand(sqlQuery, con);
-        //         command.Parameters.AddWithValue("@storeID", storeID._storeID);
-
-        //         SqlDataReader reader = command.ExecuteReader();
-
-        //         while (reader.Read())
-        //         {
-
-        //             listOfStoreFront.Add(new StoreFront()
-        //             {
-        //                 _storeID = reader.GetInt32(0),
-        //                 _storeName = reader.GetString(1),
-        //                 StoreAddress = reader.GetString(3)
-
-
-        //             });
-        //         }
-
-        //         // return listOfStoreFront[0];
-        //     }
-        //     return listOfStoreFront;
-
-        // }
 
 
         public List<StoreFront> GetAllStoreFronts()
@@ -101,6 +68,72 @@ namespace PokeDL
 
             return listOfStoreFront;
 
+        }
+
+        public List<Manager> GetAllManagers()
+        {
+
+            List<Manager> _listOfManager = new List<Manager>();
+
+            string sqlQuery = @"select * from Manager";
+
+            using (SqlConnection con = new SqlConnection(_connectionStrings))
+            {
+
+                con.Open();
+
+                SqlCommand command = new SqlCommand(sqlQuery, con);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+
+                    _listOfManager.Add(new Manager()
+                    {
+                        ManagerID = reader.GetInt32(0),
+                        ManagerName = reader.GetString(1),
+                        ManagerAddress = reader.GetString(2),
+                        ManagerEmail = reader.GetString(3),
+                        ManagerPassword = reader.GetString(4),
+                        StoreFrontID = reader.GetInt32(5)
+
+
+                    });
+                }
+            }
+
+
+            return _listOfManager;
+
+        }
+
+        public Inventory AddInventory(Inventory prodID)
+        {
+
+            string sqlQuery = @"update StoreInventory set Quantity = Quantity + 50 where prodID = @prodID";
+
+            // string sqlQuery = @"insert into sf.storeName, p.prodName, si.Quantity from StoreFront sf 
+            //     inner join StoreInventory si on sf.storeID = si.storeID 
+            //     inner join Product p on p.prodID = si.prodID";
+
+            using (SqlConnection con = new SqlConnection(_connectionStrings))
+            {
+                con.Open();
+
+                SqlCommand command = new SqlCommand(sqlQuery, con);
+                command.Parameters.AddWithValue("@prodID", prodID);
+                //command.Parameters.AddWithValue("@Quantity", _quantity);
+
+                command.ExecuteNonQuery();
+
+                // return s_inventory;
+
+
+            }
+
+            return prodID;
         }
 
 
