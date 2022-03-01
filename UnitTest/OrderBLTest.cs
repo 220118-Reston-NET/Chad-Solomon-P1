664e95a9a0501a52e74f4dcbfad9a511dfcf585a
Chad Solomon <chadcsoloman@gmail.com>
2022-03-01 11:52:53 -0500

    SearchOrderByID test

--- UnitTest/OrderBLTest.cs
+++ UnitTest/OrderBLTest.cs
@@ -11,7 +11,7 @@ namespace OrderTest
     public class CustomerBLTest
     {
         [Fact]
-        public async Task should_Get_All_Customer()
+        public async Task should_Get_All_Orders()
         {
             //Arrange
             int ID = 1;
@@ -52,6 +52,45 @@ namespace OrderTest
             Assert.Equal(orderLocation, _actualListOfOrder[0].StoreID);
             Assert.Equal(orderPrice, _actualListOfOrder[0].TotalPrice);
             Assert.Equal(customerID, _actualListOfOrder[0].CustID);
+
+        }
+
+        public async Task Should_Get_All_Orders_By_Cust_ID()
+        {
+
+            //Arrange 
+            List<Order> _listOfCustOrders = new List<Order>();
+
+            Order _newOrder = new Order()
+            {
+
+                OrderID = 1,
+                StoreID = 1,
+                TotalPrice = 20,
+                CustID = 1
+            };
+
+            _listOfCustOrders.Add(_newOrder);
+
+            Mock<IOrderRepo> _mockRepo = new Mock<IOrderRepo>();
+            _mockRepo.Setup(p => p.GetAllOrder()).ReturnsAsync(_listOfCustOrders);
+
+            IOrderBL _orderBL = new OrderBL(_mockRepo.Object);
+
+            List<Order> _expectedListOrder = new List<Order>();
+            _expectedListOrder.Add(_newOrder);
+
+            List<Order> _actualListOrder = new List<Order>();
+
+            //Act
+            _actualListOrder = await _orderBL.SearchOrder(_newOrder.CustID);
+
+            //Assert
+            Assert.Same(_expectedListOrder, _actualListOrder);
+            Assert.Same(_expectedListOrder[0].OrderID, _actualListOrder[0].OrderID);
+            Assert.Same(_expectedListOrder[1].StoreID, _actualListOrder[1].StoreID);
+            Assert.Same(_expectedListOrder[2].TotalPrice, _actualListOrder[2].TotalPrice);
+            Assert.Same(_expectedListOrder[3].CustID, _actualListOrder[3].CustID);
 
         }
     }
