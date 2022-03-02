0d133933c375ad88e8cec8bb4d7d2160992d2301
Chad Solomon <chadcsoloman@gmail.com>
2022-03-02 01:28:46 -0500

    added logging and changed addCustomer

--- FurrBApi/Controllers/CustomerController.cs
+++ FurrBApi/Controllers/CustomerController.cs
@@ -24,30 +24,38 @@ namespace FurrBApi.Controllers
         private IOrderBL _orderBL;
         private readonly IStoreFrontBL _storeBL;
         private readonly IProductBL _prodBL;
-        public CustomerController(IPokemonBL p_custBL, IOrderBL p_orderBL, IStoreFrontBL storeBL, IProductBL prodBL)
+        private readonly IInventoryBL _invBL;
+        public CustomerController(IPokemonBL p_custBL, IOrderBL p_orderBL, IStoreFrontBL storeBL, IProductBL prodBL, IInventoryBL invBL)
         {
 
             _custBL = p_custBL;
             _orderBL = p_orderBL;
             _storeBL = storeBL;
             _prodBL = prodBL;
+            _invBL = invBL;
         }
 
-        ///
-        ///
+
         /// // GET: api/Customer
         [HttpGet("GetAllCustomers")] //the ("GetAllCustomers") is changing the endpoint of this method/action
 
         //The IActionResult needs to be there to return a response essentially sying whether the action/method was succesful.
+
+        /// <summary>
+        /// Gets All Customers
+        /// </summary>
+        /// <returns>List of Customers</returns>
         public async Task<IActionResult> GetAllCustomer()
         {
             try
             {
+                Log.Information("Getting all customers");
                 return Ok(await _custBL.GetAllCustomer());
             }
             catch (SqlException)
             {
                 //will return an appropriate status code:
+                Log.Warning("error occurred in getting customer info");
                 return NotFound();
             }
         }
@@ -60,70 +68,106 @@ namespace FurrBApi.Controllers
             >We call this paramaterized Action (An action is essentially a method)
         */
 
+
+        /// <summary>
+        /// Gets All Customers by Customer ID
+        /// </summary>
+        /// <returns>JSON SIngle Customer</returns>
+
         // GET: api/Customer/5
         [HttpGet("GetCustomerByID")]
         public async Task<IActionResult> SearchCustomer([FromQuery] int id)
         {
             try
             {
-
+                Log.Information("Retrieve Customer info Based on ID");
                 return Ok(await _custBL.SearchCustomer(id));
             }
             catch (System.Exception)
             {
 
                 //will return an appropriate status code:
+                Log.Warning("Error in retrieving Customer inof by id");
                 return NotFound();
             }
         }
+
+        /// <summary>
+        /// Verifies Customer Information by Email and password in Database
+        /// </summary>
+        /// <param name="p_email"></param>
+        /// <param name="p_password"></param>
+        /// <returns>Returns JSON of Customer whos info was entered</returns>
 
         [HttpGet("VerifyCustomer")]
         public async Task<IActionResult> VerifyCustomer([FromQuery] string p_email, string p_password)
         {
             try
             {
-
+                Log.Information("Customer info was varified");
                 return Ok(await _custBL.VerifyCustomer(p_email, p_password));
             }
             catch (System.Exception)
             {
 
                 //will return an appropriate status code:
+                Log.Warning("Error in Customer Verification");
                 return NotFound();
             }
         }
+
+        /// <summary>
+        /// Searches Customer's Order History       
+        /// </summary>
+        /// <param name="custID"></param>
+        /// <returns>A List of Customer Order From database</returns>
 
         [HttpGet("CustomerOrderHistory")]
         public async Task<IActionResult> SearchOrder([FromQuery] int custID)
         {
             try
             {
-
+                Log.Information("Successfully Retrieve Customer's orders");
                 return Ok(await _orderBL.SearchOrder(custID));
             }
             catch (System.Exception e)
             {
 
                 //will return an appropriate status code:
+                Log.Warning("error in retrieving customer orders");
                 return NotFound(new { Result = e.Message });
             }
         }
+
+        /// <summary>
+        /// Filters Customer order search by Order pRice or date of order
+        /// </summary>
+        /// <param name="p_custID"></param>
+        /// <param name="p_filter"></param>
+        /// <returns>List Of Customer Orders in ascending order</returns>
 
         [HttpGet("CustomerOrderHistoryFilter")]
         public async Task<IActionResult> SearchOrderFilter([FromQuery] int p_custID, string p_filter)
         {
             try
             {
-
+                Log.Information("Successfully retrieved Custoemr Order History");
                 return Ok(await _orderBL.SearchOrderFilter(p_custID, p_filter));
             }
             catch (System.Exception)
             {
 
                 //will return an appropriate status code:
+                Log.Information("Error in retrieving Customers order history");
                 return NotFound();
             }
         }
+
+        /// <summary>
+        /// Displays the details of the order
+        /// </summary>
+        /// <param name="p_orderID"></param>
+        /// <returns>Order Details Based on order ID</returns>
 
         [HttpGet("OrderDetails")]
         public async Task<IActionResult> GetOrderDetailByOrderID([FromQuery] int p_orderID)
@@ -158,11 +202,12 @@ namespace FurrBApi.Controllers
                 }
                 _orderDetails.TotalPrice = _order.TotalPrice;
                 _orderDetails.OrderTime = _order.TimeStamp;
+                Log.Information("Order details retrieved");
                 return Ok(_orderDetails);
 
 
             }
-
+            Log.Information("Order was not found");
             return NotFound(new { Result = "Order not found" });
 
         }
@@ -173,43 +218,83 @@ namespace FurrBApi.Controllers
             IF you need a bunch of information though like for when a customer might be creating an account, YOu would use this format B/C otherwise we would need to specifiy each element needed from the customer {elem}/{elem}/{elem}/ which would take a lot of time.
         */
 
+        /// <summary>
+        /// Adds a customer to the customer table in the database
+        /// </summary>
+        /// <returns>Customer info that will be in database</returns>
+
         // POST: api/Customer
-        [HttpPost] //Post sends data to the server. So here we are sending customer information to the server.
-                   //we are obtaining the customer info via a form body.
-        public async Task<IActionResult> Post([FromQuery] Customer p_cust)
+        [HttpPost("AddCustomer")] //Post sends data to the server. So here we are sending customer information to the server.
+                                  //we are obtaining the customer info via a form body.
+        public async Task<IActionResult> Post([FromBody] Customer p_cust)
         {
 
             try
             {
+                Log.Information("Successfully added Customer");
                 return Ok(await _custBL.AddCustomer(p_cust));
             }
             catch (System.Exception)
             {
-
+                Log.Information("Error in adding Customer");
                 return Conflict();
             }
         }
 
+
+        /// <summary>
+        /// Places and order to the order table in the database
+        /// </summary>
+        /// <param name="_orderLocation"></param>
+        /// <param name="_custID"></param>
+        /// <param name="_cart"></param>
+        /// <returns>Returns whether order was placed successfully</returns>
 
         [HttpPost("PlaceOrder")]
 
         public IActionResult Post([FromQuery] int _orderLocation, int _custID, List<LineItems> _cart)
         {
+            //List<Inventory> _listOfInventory = new List<Inventory>();
+            //Inventory _inventory = new Inventory();
+            //_listOfInventory = _invBL.GetAllInventoryByStoreID(_orderLocation);
+
+            // foreach (var item in _listOfInventory)
+            // {
+            //     _listOfInventory.Find(p => p.ProductID.Equals(_cart[0].Product == _cart[0].Quantity));
+            // if (_listOfInventory[0].Quantity < _cart[0].Quantity)
+            // {
+
+            //     Console.WriteLine("sorry we do not have that item");
+            //     return Conflict();
+
+            // }
+            // }
+
+            // _listOfInventory = _invBL.GetAllInventoryByStoreID(_orderLocation);
+
+
+            // if (_listOfInventory.Find(p => p.ProductID == _cart[0].Product))
+
 
             try
             {
+                Log.Information("Order was successful");
                 _orderBL.AddOrder(_orderLocation, _custID, _cart);
                 return Ok(new { Result = "Place order Successful" });
             }
             catch (System.Exception)
             {
-
+                Log.Information("Error in placing order");
                 return Conflict();
             }
         }
 
 
 
+        /// <summary>
+        /// Updates Customer info in database
+        /// </summary>
+        /// <returns>Returns updated info</returns> 
         // PUT: api/Customer/5
         [HttpPut("UpdateCustomer")] //PUT creates a new resource or replaces a representation of the target resource with 
                                     //the target payload.
@@ -217,15 +302,16 @@ namespace FurrBApi.Controllers
         {
 
 
-            // try
-            // {
-            return Ok(await _custBL.UpdateCustomer(p_cust));
-            // }
-            // catch (System.Exception exc)
-            // {
-
-            //     return Conflict(exc.Message);
-            // }
+            try
+            {
+                Log.Information("Customer info was updated");
+                return Ok(await _custBL.UpdateCustomer(p_cust));
+            }
+            catch (System.Exception exc)
+            {
+                Log.Information("Error in updating Customer Info");
+                return Conflict(exc.Message);
+            }
 
         }
 
